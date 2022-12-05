96b77e50303c621603cea74ad4210e48b8e68512
Miguel Lopez <ing.miguellv@gmail.com>
2022-12-05 17:48:39 -0600

    agrego tabla con visualizacion de data de los clientes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from 'react-router-dom'
+import Cliente from '../components/Cliente'
 
 export function loader() {
   const clientes = [
@@ -48,7 +49,34 @@ const Index = () => {
   return (
     <>
       <h1 className="text-4xl font-black text-blue-900">Clientes</h1>
-      <p className="mt-3">Adminsitra tus clientes</p>
+      <p className="mt-3">Administra tus clientes</p>
+
+      {
+        clientes.length ? (
+          <table className="w-full bg-white shadow mt-5 table-auto">
+            <thead className="bg-blue-800 text-white">
+              <tr>
+                <th className="p-2">Cliente</th>
+                <th className="p-2">Contacto</th>
+                <th className="p-2">Acciones</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {clientes.map( cliente => (
+                <Cliente
+                  cliente={ cliente }
+                  key={cliente.id}
+                />
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p className="mt-10 text-center">No hay clientes aún</p>
+        )
+      }
+
+      
     </>
   )
 }
